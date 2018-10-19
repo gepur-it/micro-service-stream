611fbc69285782e80990c85a5a202a84a334115c
zogxray <zogxray@gmail.com>
2018-10-19 15:32:11 +0300

    master not check message

--- query.go
+++ query.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"github.com/sirupsen/logrus"
-)
-
-type AppealStreamMessage struct {
-	AppealID   string  `json:"appealId"`
-	ManagerId  *string `json:"managerId"`
-	Message    string  `json:"message"`
-	Action     string  `json:"action"`
-	ActionAt   string  `json:"actionAt"`
-	ReceivedAt string  `json:"receivedAt"`
-}
-
 func (h *Hub) query() {
 	msgs, err := AMQPChannel.Consume(
 		"erp_to_socket_appeal",
@@ -30,30 +16,7 @@ func (h *Hub) query() {
 
 	go func() {
 		for d := range msgs {
-			appealStreamMessage := &AppealStreamMessage{}
-			err := json.Unmarshal(d.Body, &appealStreamMessage)
-
-			if err != nil {
-				logger.WithFields(logrus.Fields{
-					"error": err,
-				}).Error("Can`t decode query callBack:")
-			}
-
-			bytesToSend, err := json.Marshal(appealStreamMessage)
-
-			if err != nil {
-				logger.WithFields(logrus.Fields{
-					"error": err,
-				}).Error("Can`t encode query callBack:")
-			}
-
-			h.broadcast <- bytesToSend
-
-			logger.WithFields(logrus.Fields{
-				"appeal": appealStreamMessage.AppealID,
-				"action": appealStreamMessage.Action,
-			}).Info("Read message from query:")
-
+			h.broadcast <- d.Body
 			d.Ack(false)
 		}
 	}()
